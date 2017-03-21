b0e46c817c769af96211e49855f23e3de16a6e4e
Thierry Delbart <thierry@thde.pro>
2017-03-20 21:45:41 -0300

    Update server with /api/hubs route

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // PARSING COMPANIES FILE
 let companies = [];
+let hubs = [];
 const csv=require('csvtojson');
 csv()
 .fromFile('./companies.csv')
 .on('json',(jsonObj)=>{
-    // combine csv header row and csv line to a json object 
-    // jsonObj.a ==> 1 or 4 
-
     companies.push(jsonObj);
 })
 .on('done',(error)=>{
@@ -19,14 +20,39 @@ csv()
   } else {
     console.log('Companies parsed successfully!');
 
+    csv()
+    .fromFile('./hubs.csv')
+    .on('json',(jsonObj)=>{
+        hubs.push(jsonObj);
+    })
+    .on('done',(error)=>{
+      if (error) {
+        console.log("Error while parsing hubs");
+      } else {
+        console.log('Hubs parsed successfully!');
 
-    // ROUTES
-    app.get('/api/companies', (req,res) => {
-      res.json({ companies });
-    });
+        // ROUTES
+        app.get('/api/companies', (req,res) => {
+          res.json({ companies });
+        });
 
-    // START SERVER
-    let port = process.env.port || 8080;
-    app.listen(port, () => ('Server is running on port ' + port));    
+        app.get('/api/hubs', (req,res) => {
+          res.json({ hubs });
+        });
+
+        app.post('/api/order', (req,res) => {
+          console.log(req.body.order);
+          if (req.body.order)
+            res.json({ status: "OK", order: req.body.order });
+          else
+            res.json({ status: "ERROR" });
+        });
+
+        // START SERVER
+        let port = process.env.port || 8080;
+        console.log('Server is running on port ' + port);
+        app.listen(port, () => ('Server is running on port ' + port));
+      }
+    })
   }
 })
